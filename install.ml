f38f25786b15732881c6c5ea173b216dd742c381
Jeremie Dimino <jdimino@janestreet.com>
2016-09-16 15:04:53 +0100

    114.10+84

--- install.ml
+++ install.ml
@@ -6,5 +6,6 @@ open Oasis2opam_install;;
 generate ~package:"core_bench"
   [ oasis_lib "core_bench"
   ; oasis_lib "inline_benchmarks_public"
+  ; oasis_obj "public_runner"
   ; file "META" ~section:"lib"
   ]
