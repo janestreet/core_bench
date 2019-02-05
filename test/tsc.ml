2adc3fc6bdc18e318a477b87401fce00c36e422b
Xavier Clerc <xclerc@janestreet.com>
2019-02-05 10:09:03 +0000

    v0.12-preview.120.22+10

--- test/tsc.ml
+++ test/tsc.ml
@@ -1,12 +1,14 @@
 open Core
 open Core_bench.Std
 
+let calibrator = force Time_stamp_counter.calibrator
+
 let command = Bench.make_command [
   Bench.Test.create ~name:"Time.now" (fun () ->
     ignore (Time.now ()));
 
   Bench.Test.create ~name:"Calibrator.calibrate" (fun () ->
-    ignore (Time_stamp_counter.Calibrator.calibrate ()));
+    ignore (Time_stamp_counter.Calibrator.calibrate calibrator));
 
   Bench.Test.create_group ~name:"Perf" [
     Bench.Test.create ~name:"TSC.now" (fun () ->
@@ -14,19 +16,16 @@ let command = Bench.make_command [
 
     (let c = Time_stamp_counter.now () in
      Bench.Test.create ~name:"TSC.to_time" (fun () ->
-       ignore (Time_stamp_counter.to_time c)));
+       ignore (Time_stamp_counter.to_time c ~calibrator)));
 
     Bench.Test.create ~name:"TSC.to_time (TSC.now ())" (fun () ->
-      ignore (Time_stamp_counter.to_time (Time_stamp_counter.now ())));
+      ignore (Time_stamp_counter.to_time (Time_stamp_counter.now ()) ~calibrator));
 
     (let c = Time_stamp_counter.now () in
      Bench.Test.create ~name:"TSC.to_nanos_since_epoch" (fun () ->
-       ignore (Time_stamp_counter.to_time_ns c)));
+       ignore (Time_stamp_counter.to_time_ns c ~calibrator)));
 
     Bench.Test.create ~name:"TSC.to_nanos_since_epoch(TSC.now ())" (fun () ->
-      ignore (Time_stamp_counter.to_time_ns
-                (Time_stamp_counter.now ())));
+      ignore (Time_stamp_counter.to_time_ns (Time_stamp_counter.now ()) ~calibrator));
   ];
 ]
-
-
